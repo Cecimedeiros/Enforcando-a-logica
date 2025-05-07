225791b7f4cd8c51bee2fef7a4fdeed8a202ef3b
Cecilia Medeiros <souzaceci22@gmail.com>
2025-05-07 09:08:52 -0300

    desenvolvendo função e implementando cli-lib

---
+++ projeto-pif/include/cli-lib.h
@@ -0,0 +1,10 @@
+#ifndef CLI_LIB_H
+#define CLI_LIB_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char** processamento_palavras(const char* filename, int* qtdpalavras);
+
+#endif
--- projeto-pif/scr/main.c
+++ projeto-pif/scr/main.c
@@ -1,1 +1,5 @@
-
+#include "../include/cli-lib.h"
+#include <string.h>
+int main(){
+    
+}
